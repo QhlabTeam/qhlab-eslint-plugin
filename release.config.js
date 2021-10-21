139a4af83446b5fdc7581485f539eb20c43910e7
Yoki Yu <yokiyuqian@gmail.com>
2021-10-21 12:45:26 +0800

    chore: add semantic-release

---
+++ release.config.js
@@ -0,0 +1,47 @@
+/** @type {import('semantic-release').Options} */
+const release = {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+        writerOpts: {
+          commitsSort: ['subject', 'scope'],
+        },
+      },
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['dist/**/*.{js,css}', 'docs', 'package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+}
+
+module.exports = {
+  release,
+}
